12bd75ae4f21db607e4212783f643de9b89d936a
Vishal Sharma <785vishal443@gmail.com>
2022-12-20 23:05:08 +0530

    Create JsFactoryFunctions.js

---
+++ JsFactoryFunctions.js
@@ -0,0 +1,24 @@
+// Javascript Factory Functions 
+
+// 
+// the functions return object 
+const JSFactoryFunctions =()=> {
+return {
+    name: "TIM TOM",
+    lastName: "chin chon"
+}
+}
+
+
+function JSFfunction() {
+
+}
+
+const jsObject = JSFactoryFunctions();
+console.log(jsObject);
+
+
+
+
+
+
