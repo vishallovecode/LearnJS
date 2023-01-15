33f38ca3ddbda6dbb699eebeb5654f08a10e6540
Vishal Sharma <785vishal443@gmail.com>
2023-01-15 10:38:03 +0530

    Update Fetch.js

--- Fetch.js
+++ Fetch.js
@@ -16,11 +16,20 @@
 
 //
 
- fetch('https://jsonplaceholder.typicode.com/posts').then((res)=>{
+const rootContainer = document.createElement('div');
+rootContainer.style.border = "1px solid";
+rootContainer.style.background = "lightgreen"
+rootContainer.style.padding= "20px"
+rootContainer.style.width = '95%'
+rootContainer.style.display = "flex"
+rootContainer.style.flexWrap = 'wrap'
+document.body.appendChild(rootContainer);
+
+
+ fetch('https://jsonplaceholder.typicode.com/posts' , null , 'GET').then((res)=>{
     if(res.status==200) {
        res.json().then(response=>{
-        console.log(response)
-        throw("Hey chill out")
+         createCard(response)
         }).catch((error)=>{
             console.log(error)
         })
@@ -28,3 +37,47 @@
  }).catch((error)=>{
     console.log(error)
  })
+
+
+ // creating card in dom  
+
+ 
+
+ const createCard =(postData) => {
+   postData.forEach((element , index) => {
+      console.log(postData)
+      const title =  element.title;
+      const body =  element.body;
+      const cardContainer  = document.createElement('div');
+      cardContainer.style.border = '1px solid blue'
+      cardContainer.style.minHeight= "100px"
+      cardContainer.style.maxHeight= "200px"
+      cardContainer.style.minWidth ="300px"
+      cardContainer.style.maxWidth ="300px"
+      cardContainer.style.display = "flex";
+      cardContainer.style.flexDirection = 'column';
+      cardContainer.style.margin = '20px'
+      cardContainer.style.padding = '10px'
+    
+    
+   
+      const span1 = document.createElement('span');
+      span1.style.fontWeight = "bold";
+      span1.style.fontSize= "20px"
+      span1.innerText = title;
+      cardContainer.appendChild(span1)
+      const span2 = document.createElement('span');
+      span2.style.fontWeight = "semibold";
+      span2.style.fontSize= "14px"
+      span2.innerText = body;
+      span2.style.color = 'blue'
+      span2.style.margin = '5px'
+      cardContainer.appendChild(span2)
+      rootContainer.appendChild(cardContainer)
+   
+    });
+ }
+
+ 
+ 
+
