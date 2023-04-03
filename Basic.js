72c4f9a043774bdf721a33a584bdc0ae3d58bc7b
Vishal Sharma <vishalsharma@Vishals-MacBook-Pro.local>
2023-04-03 23:33:39 +0530

    basi1 new content

--- Basic.js
+++ Basic.js
@@ -79,3 +79,148 @@ console.log(parseInt(1234.555));
 
 const name1 = "Vishal";
 name1 = "Vishal"; //  this will throw error because you cannot reaasign the const variable
+
+
+
+// New Code
+
+
+
+
+console.log("Hey will do later..."); 
+console.log("Hey I am learning Js");
+console.log("Requesting  evryone to code parallel")
+
+
+ var abc = 'CHIM CHOM';
+ var abc1 = 'Chim Chom2';
+
+ console.log(abc);
+
+ console.log(abc1);
+
+ var mult1= 'maze me';
+
+
+ var multi2 = "funny ha ";
+
+
+ console.log(mult1 , multi2);
+
+
+ // console  value and variable sath me  
+
+ console.log("Hey your name is ::" ,  abc , "," , 'Hey updated name is::' , abc1);
+
+
+
+ // typeof 
+
+  console.log( "Data type of multi2 is::"  ,typeof multi2 , 'data type of multi1::' , typeof mult1) //  string
+
+
+
+
+
+  var a = 20;
+  var b= 30;
+
+  console.log(40)
+  console.log(a , b)
+
+  console.log(typeof a , typeof b)
+
+  console.log(typeof -1)
+
+  var c = a+b;
+console.log(typeof c)
+
+console.log(1/0); // Infinity  => NaN isNaN();
+console.log(typeof 1/0);
+
+
+console.log(typeof Infinity , 'hey')
+
+
+
+// Primitive Datatype
+//  string , number , boolean  , undefined  , null  ,  ,     =>>> Symbol ,BigInt ()
+
+console.log(typeof Boolean); // Function
+console.log(typeof Number); // // Function
+console.log(typeof String); //// Function
+
+
+var dataType =  typeof 'hello'; // string
+
+console.log(typeof dataType)
+
+
+console.log(typeof undefined) // undefined
+
+console.log(typeof null) 
+
+ var checked = true; // booolean
+
+ console.log( typeof typeof checked)
+
+
+
+ checked = "Hey I am converted"; // strinf
+
+console.log('newtype' ,typeof checked)
+
+
+checked = 1234;
+
+console.log(typeof checked);
+
+checked = ()=> {
+    console.log("yeee");
+}
+
+
+console.log(typeof checked);
+
+
+
+lastName = "Hello";
+console.log(lastName)
+ 
+
+
+
+
+
+
+
+// isNaN() => THIS FUNCTION 
+
+
+var p = "1234"; // typeof p => string;
+
+var pp = 'hey'
+var t = Number('p');
+
+var tt = Number(pp)
+console.log(t ,'hey');
+console.log(typeof NaN)
+console.log(typeof tt);
+
+
+console.log(typeof  +'12345') ; // shorthan way to convert number string to number
+
+
+var isNumber = isNaN('123456'); // FALSE
+
+var isNumber2 = isNaN('hello'); // TRUE
+
+
+console.log(isNumber)
+console.log(isNumber2)
+
+
+
+
+
+
