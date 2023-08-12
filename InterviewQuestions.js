fbc1a9e2f248e4061c79de340c400e82f67b4c73
Vishal Sharma <785vishal443@gmail.com>
2023-08-12 09:33:34 +0530

    Update InterviewQuestions.js

---
+++ InterviewQuestions.js
@@ -0,0 +1,12 @@
+// What is Hoisting?
+// How JS code executed?
+// What is execution context ?
+// What is Scope?
+// What is lexical scope ?
+// What is diff b/w let var and const?
+// What is function statement?
+// what is function expression?
+// what is IIF (immediately invoked function)
+// Which value in js in not defined ?
+// what is defined value and not defined value ?
+// Diff b/w null and undefined
