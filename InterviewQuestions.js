9cb55ac780a57161dd407e2db186b582dcf018af
Vishal Sharma <vishal.sharma@zeptonow.com>
2023-08-13 14:13:09 +0530

    null vs undefined

--- InterviewQuestions.js
+++ InterviewQuestions.js
@@ -1,118 +1,198 @@
-// What is Hoisting? done
-// How JS code executed?
-// What is execution context ? done
-// What is Scope?
-// What is lexical scope ?
-// What is diff b/w let var and const? done
-// What is function statement?
-// what is function expression?
-// what is IIF (immediately invoked function)
-// Which value in js in not defined ? done
-// what is defined value and not defined value ? done
-// Diff b/w null and undefined
-// What is Hoisting? done
-// What is tdz? done
+// // What is Hoisting? done
+// // How JS code executed?
+// // What is execution context ? done
+// // What is Scope?
+// // What is lexical scope ?
+// // What is diff b/w let var and const? done
+// // What is function statement?
+// // what is function expression?
+// // what is IIF (immediately invoked function)
+// // Which value in js in not defined ? done
+// // what is defined value and not defined value ? done
+// // Diff b/w null and undefined
+// // What is Hoisting? done
+// // What is tdz? done
 
-// does let and const are hoisted ? done
+// // does let and const are hoisted ? done
 
-// Js Engine creates a speacial environoment to handle all the js code like ,
-// parshing , memory allocation , transformation and execution ,
-// this environment is called the execution context
+// // Js Engine creates a speacial environoment to handle all the js code like ,
+// // parshing , memory allocation , transformation and execution ,
+// // this environment is called the execution context
 
-console.log(a); // undefined
-var a = 20;
+// console.log(a); // undefined
+// var a = 20;
 
-//TDZ
+// //TDZ
 
-//  The area or block  from where a let and  const
-// cannot be accesssible until the Js engine does not initilize or declare the varible
+// //  The area or block  from where a let and  const
+// // cannot be accesssible until the Js engine does not initilize or declare the varible
 
-console.log(a1); // undefined
-// console.log(b1);
-// console.log(c1);
+// console.log(a1); // undefined
+// // console.log(b1);
+// // console.log(c1);
 
-var a1 = 30;
-let b1 = 40;
-const c1 = 80;
-let d1;
+// var a1 = 30;
+// let b1 = 40;
+// const c1 = 80;
+// let d1;
 
-// var name1 = "Vishal";
-// {
-//   var name1 = "Sharma";
-//     let name1 = "Verma";
-//   console.log(name1); // verma
-//   // const name ="Sharma1"
+// // var name1 = "Vishal";
+// // {
+// //   var name1 = "Sharma";
+// //     let name1 = "Verma";
+// //   console.log(name1); // verma
+// //   // const name ="Sharma1"
+// // }
+
+// // console.log(name1);
+// // Vishal , 'Sharma1, 'error'
+
+// // var=> redeclared , re assign  ,
+// // let => variable cann be re assig but cannot re-declared
+
+// // let name2 = "Sharma";
+// // // var name2= "Sharma"
+// // name2 = "Raj";
+
+// // console.log(name2);
+
+// // var nam3 = "ss";
+
+// // {
+// //   nam3 = "zzz";
+// //   var nam3 = "ZZZZ";
+// //   let nam3 = "ccccc"
+// // }
+
+// //  var can be redeclared and reassign
+// // let cannot be re declared any variable in same scope have declared with let  you cannot re-declared that with var and const
+// // let variable can be re- assign
+// // const variable cannot be re-assign and cannot be re-declared
+// // const variable need to initilize at the time declaration
+
+// // const cc; erro
+// const cc = "Vishal";
+// // cc="name" error
+
+// const call1224 = (callback) => {
+//   //     callback  = (a, b) => {
+//   //   return a + b;
+//   // };
+
+//   return callback(1, 2); // 3
+// };
+
+// const sum = (a, b) => {
+//   return a + b;
+// };
+
+// // call1224(sum(5, 6)); // call1224(11)
+
+// const data = call1224(sum); // here i am passing function as a reference , i am not calling the function
+// console.log(data);
+
+// // IIF
+
+// // The function invoked at the time of declaration
+
+// (function IIF() {
+//   console.log("Hey I am IIf");
+// })();
+
+// //console.log(IIF()); // this will throw error
+
+// const callme123 = () => {
+//   console.log("hey");
+// };
+
+// // (
+// //     callme123
+// // )() this is not iif becuuse here we are using callme123 which is already declared
+
+// (() => {
+//   console.log("Hey Arrow , iif");
+// })();
+
+// (function () {
+//   console.log("Hey I am anonymousx");
+// })();
+
+// Function execution context
+
+// var a = 30;
+// let b = 60;
+// const c = 80;
+
+// function getId() {
+//   console.log(a);
+//   //   console.log(b);
+//   //   console.log(c);
+//   var a = 90;
+//   let b = 900;
+//   const c = 600;
+//   return undefined;
 // }
 
-// console.log(name1);
-// Vishal , 'Sharma1, 'error'
+// const result = getId();
 
-// var=> redeclared , re assign  ,
-// let => variable cann be re assig but cannot re-declared
+// console.log(result);
 
-// let name2 = "Sharma";
-// // var name2= "Sharma"
-// name2 = "Raj";
+// console.log(a, b, c);
 
-// console.log(name2);
+// function statment , declaration
+//  function expression
 
-// var nam3 = "ss";
+// console.log(getName);
+// getName();
 
-// {
-//   nam3 = "zzz";
-//   var nam3 = "ZZZZ";
-//   let nam3 = "ccccc"
+// console.log(getName3);
+// getName3();
+
+// console.log(getName1);
+// getName1();
+
+// console.log(getName2);
+// getName2();
+
+// // FUNCTION DECLARATION
+// function getName() {
+//   console.log("Hey");
 // }
 
-//  var can be redeclared and reassign
-// let cannot be re declared any variable in same scope have declared with let  you cannot re-declared that with var and const
-// let variable can be re- assign
-// const variable cannot be re-assign and cannot be re-declared
-// const variable need to initilize at the time declaration
+// const getName1 = () => {
+//   console.log("Hey, with const arrow function");
+// };
 
-// const cc; erro
-const cc = "Vishal";
-// cc="name" error
+// let getName2 = () => {
+//   console.log("Hey, with let arrow function");
+// };
 
-const call1224 = (callback) => {
-  //     callback  = (a, b) => {
-  //   return a + b;
-  // };
+// var getName3 = () => {
+//   console.log("Hey, with var arrow function");
+// };
 
-  return callback(1, 2); // 3
-};
+// const getiDS = function () {
+//   console.log("Hey");
+// };
+// 2X+3Y+Z =20; // EXPRESSION
 
-const sum = (a, b) => {
-  return a + b;
-};
+// null undefined
 
-// call1224(sum(5, 6)); // call1224(11)
+//undefined means value which is not defined
+// undefined is flasy value
+//typeof undefined =>
+// this value is assigned by the compiler at the time of memor allocation
+console.log(typeof undefined); // undefined
 
-const data = call1224(sum); // here i am passing function as a reference , i am not calling the function
-console.log(data);
+var aaa; // undefined
 
-// IIF
+// Null means falsy value
+// it s assigned by developer
+// its a one type of value
+// typeof null
+console.log(typeof null);
 
-// The function invoked at the time of declaration
+console.log("==", null == undefined);
+console.log("==", true == "vishal");
 
-(function IIF() {
-  console.log("Hey I am IIf");
-})();
-
-//console.log(IIF()); // this will throw error
-
-const callme123 = () => {
-  console.log("hey");
-};
-
-// (
-//     callme123
-// )() this is not iif becuuse here we are using callme123 which is already declared
-
-(() => {
-  console.log("Hey Arrow , iif");
-})();
-
-(function () {
-  console.log("Hey I am anonymousx");
-})();
+console.log("===", null === undefined);
