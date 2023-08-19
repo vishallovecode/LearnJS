e74f0fc30f7efb9ae5a9f5ab0781edc3d06b6eb4
Vishal Sharma <vishal.sharma@zeptonow.com>
2023-08-19 10:14:36 +0530

    function statement and function expression

--- InterviewQuestions.js
+++ InterviewQuestions.js
@@ -1,198 +1,271 @@
-// // What is Hoisting? done
-// // How JS code executed?
-// // What is execution context ? done
-// // What is Scope?
-// // What is lexical scope ?
-// // What is diff b/w let var and const? done
-// // What is function statement?
-// // what is function expression?
-// // what is IIF (immediately invoked function)
-// // Which value in js in not defined ? done
-// // what is defined value and not defined value ? done
-// // Diff b/w null and undefined
-// // What is Hoisting? done
-// // What is tdz? done
+// // // What is Hoisting? done
+// // // How JS code executed? // done
+// // // What is execution context ? done
+// // // What is Scope? // done
+// // // What is lexical scope ? // done
+// // // What is diff b/w let var and const? done
+// // // What is function statement? // done
+// // // what is function expression?// done
+// // // what is IIF (immediately invoked function)
+// // // Which value in js in not defined ? done
+// // // what is defined value and not defined value ? done
+// // // Diff b/w null and undefined
+// // // What is Hoisting? done
+// // // What is tdz? done
 
-// // does let and const are hoisted ? done
+// // // does let and const are hoisted ? done
 
-// // Js Engine creates a speacial environoment to handle all the js code like ,
-// // parshing , memory allocation , transformation and execution ,
-// // this environment is called the execution context
+// // // Js Engine creates a speacial environoment to handle all the js code like ,
+// // // parshing , memory allocation , transformation and execution ,
+// // // this environment is called the execution context
 
-// console.log(a); // undefined
-// var a = 20;
+// // console.log(a); // undefined
+// // var a = 20;
 
-// //TDZ
+// // //TDZ
 
-// //  The area or block  from where a let and  const
-// // cannot be accesssible until the Js engine does not initilize or declare the varible
+// // //  The area or block  from where a let and  const
+// // // cannot be accesssible until the Js engine does not initilize or declare the varible
 
-// console.log(a1); // undefined
-// // console.log(b1);
-// // console.log(c1);
+// // console.log(a1); // undefined
+// // // console.log(b1);
+// // // console.log(c1);
 
-// var a1 = 30;
-// let b1 = 40;
-// const c1 = 80;
-// let d1;
+// // var a1 = 30;
+// // let b1 = 40;
+// // const c1 = 80;
+// // let d1;
 
-// // var name1 = "Vishal";
-// // {
-// //   var name1 = "Sharma";
-// //     let name1 = "Verma";
-// //   console.log(name1); // verma
-// //   // const name ="Sharma1"
+// // // var name1 = "Vishal";
+// // // {
+// // //   var name1 = "Sharma";
+// // //     let name1 = "Verma";
+// // //   console.log(name1); // verma
+// // //   // const name ="Sharma1"
+// // // }
+
+// // // console.log(name1);
+// // // Vishal , 'Sharma1, 'error'
+
+// // // var=> redeclared , re assign  ,
+// // // let => variable cann be re assig but cannot re-declared
+
+// // // let name2 = "Sharma";
+// // // // var name2= "Sharma"
+// // // name2 = "Raj";
+
+// // // console.log(name2);
+
+// // // var nam3 = "ss";
+
+// // // {
+// // //   nam3 = "zzz";
+// // //   var nam3 = "ZZZZ";
+// // //   let nam3 = "ccccc"
+// // // }
+
+// // //  var can be redeclared and reassign
+// // // let cannot be re declared any variable in same scope have declared with let  you cannot re-declared that with var and const
+// // // let variable can be re- assign
+// // // const variable cannot be re-assign and cannot be re-declared
+// // // const variable need to initilize at the time declaration
+
+// // // const cc; erro
+// // const cc = "Vishal";
+// // // cc="name" error
+
+// // const call1224 = (callback) => {
+// //   //     callback  = (a, b) => {
+// //   //   return a + b;
+// //   // };
+
+// //   return callback(1, 2); // 3
+// // };
+
+// // const sum = (a, b) => {
+// //   return a + b;
+// // };
+
+// // // call1224(sum(5, 6)); // call1224(11)
+
+// // const data = call1224(sum); // here i am passing function as a reference , i am not calling the function
+// // console.log(data);
+
+// // // IIF
+
+// // // The function invoked at the time of declaration
+
+// // (function IIF() {
+// //   console.log("Hey I am IIf");
+// // })();
+
+// // //console.log(IIF()); // this will throw error
+
+// // const callme123 = () => {
+// //   console.log("hey");
+// // };
+
+// // // (
+// // //     callme123
+// // // )() this is not iif becuuse here we are using callme123 which is already declared
+
+// // (() => {
+// //   console.log("Hey Arrow , iif");
+// // })();
+
+// // (function () {
+// //   console.log("Hey I am anonymousx");
+// // })();
+
+// // Function execution context
+
+// // var a = 30;
+// // let b = 60;
+// // const c = 80;
+
+// // function getId() {
+// //   console.log(a);
+// //   //   console.log(b);
+// //   //   console.log(c);
+// //   var a = 90;
+// //   let b = 900;
+// //   const c = 600;
+// //   return undefined;
 // // }
 
-// // console.log(name1);
-// // Vishal , 'Sharma1, 'error'
+// // const result = getId();
 
-// // var=> redeclared , re assign  ,
-// // let => variable cann be re assig but cannot re-declared
+// // console.log(result);
 
-// // let name2 = "Sharma";
-// // // var name2= "Sharma"
-// // name2 = "Raj";
+// // console.log(a, b, c);
 
-// // console.log(name2);
+// // function statment , declaration
+// //  function expression
 
-// // var nam3 = "ss";
+// // console.log(getName);
+// // getName();
 
-// // {
-// //   nam3 = "zzz";
-// //   var nam3 = "ZZZZ";
-// //   let nam3 = "ccccc"
+// // console.log(getName3);
+// // getName3();
+
+// // console.log(getName1);
+// // getName1();
+
+// // console.log(getName2);
+// // getName2();
+
+// // // FUNCTION DECLARATION
+// // function getName() {
+// //   console.log("Hey");
 // // }
 
-// //  var can be redeclared and reassign
-// // let cannot be re declared any variable in same scope have declared with let  you cannot re-declared that with var and const
-// // let variable can be re- assign
-// // const variable cannot be re-assign and cannot be re-declared
-// // const variable need to initilize at the time declaration
+// // const getName1 = () => {
+// //   console.log("Hey, with const arrow function");
+// // };
 
-// // const cc; erro
-// const cc = "Vishal";
-// // cc="name" error
+// // let getName2 = () => {
+// //   console.log("Hey, with let arrow function");
+// // };
 
-// const call1224 = (callback) => {
-//   //     callback  = (a, b) => {
-//   //   return a + b;
-//   // };
+// // var getName3 = () => {
+// //   console.log("Hey, with var arrow function");
+// // };
 
-//   return callback(1, 2); // 3
+// // const getiDS = function () {
+// //   console.log("Hey");
+// // };
+// // 2X+3Y+Z =20; // EXPRESSION
+
+// // null undefined
+
+// //undefined means value which is not defined
+// // undefined is flasy value
+// //typeof undefined =>
+// // this value is assigned by the compiler at the time of memor allocation
+// console.log(typeof undefined); // undefined
+
+// var aaa; // undefined
+
+// // Null means falsy value
+// // it s assigned by developer
+// // its a one type of value
+// // typeof null
+// console.log(typeof null);
+
+// console.log("==", null == undefined);
+// console.log("==", true == "vishal");
+
+// console.log("===", null === undefined);
+
+// // function statement/declration and function expression ??
+
+// // expression
+// // 2x+2y+z=40;
+
+// // If you are assigning the any function that can be arrow function and normal functions
+// // that is called function expression where where variable which is taking the value of whole function is hoisted like another normal value
+
+// // here getEmployeeDetail is the variable type const
+
+// // getEmployeeDetail(); // reference error
+// // here  getEmployeeDetails2 is var varibale and value of that in this cases will be undefined and
+// // if you try to call undefined as  functio  this will throw the error undefined is not a function
+// // getEmployeeDetails2(); // undefined
+// // getEmployeDetails3(); // reference error
+// // all above method  are function expression
+
+// // getAll is function statement  or declaration
+// console.log(getAll, "getall");
+// getAll();
+
+// const getEmployeeDetail = () => {
+//   console.log("this is the employee details....");
 // };
 
-// const sum = (a, b) => {
-//   return a + b;
+// console.log(typeof getEmployeeDetail); // function
+
+// var getEmployeeDetails2 = () => {
+//   console.log("this is the employee details1....");
 // };
 
-// // call1224(sum(5, 6)); // call1224(11)
-
-// const data = call1224(sum); // here i am passing function as a reference , i am not calling the function
-// console.log(data);
-
-// // IIF
-
-// // The function invoked at the time of declaration
-
-// (function IIF() {
-//   console.log("Hey I am IIf");
-// })();
-
-// //console.log(IIF()); // this will throw error
-
-// const callme123 = () => {
-//   console.log("hey");
+// let getEmployeDetails3 = function () {
+//   console.log("hey normal function");
 // };
 
-// // (
-// //     callme123
-// // )() this is not iif becuuse here we are using callme123 which is already declared
-
-// (() => {
-//   console.log("Hey Arrow , iif");
-// })();
-
-// (function () {
-//   console.log("Hey I am anonymousx");
-// })();
-
-// Function execution context
-
-// var a = 30;
-// let b = 60;
-// const c = 80;
-
-// function getId() {
-//   console.log(a);
-//   //   console.log(b);
-//   //   console.log(c);
-//   var a = 90;
-//   let b = 900;
-//   const c = 600;
-//   return undefined;
+// // function declaration
+// // the function name variable is taken whole  the value as initial value
+// function getAll() {
+//   console.log("hey function statement");
 // }
 
-// const result = getId();
+// getEmployeeDetail();
+// getAll();
+// getEmployeeDetails2();
+// getEmployeDetails3();
 
-// console.log(result);
+function callMe() {
+  console.log(arguments); //
+}
 
-// console.log(a, b, c);
+// in the case of arrow you cannot access the arguments inside the function
+// it will say arguments is not defined
+const callMe1 = () => {
+  //   console.log("arguments", arguments); // error
+};
 
-// function statment , declaration
-//  function expression
+// In javascript you can pass any arguments does matter how many parameter function accepting
 
-// console.log(getName);
-// getName();
+callMe(1234, 3456, 789); // / arguments.length  =>3
+callMe(); // arguments.length  =>0
+callMe1("vishal", "sharma", 888888);
 
-// console.log(getName3);
-// getName3();
+// how to know the function name  ??
 
-// console.log(getName1);
-// getName1();
+function Myname(param1, param2, param3) {
+  console.log("My name  I am cool");
+}
 
-// console.log(getName2);
-// getName2();
+console.log(Myname.name);
+console.log(Myname.length); // functionName.length will give the count of parameter that particular function is accepting
 
-// // FUNCTION DECLARATION
-// function getName() {
-//   console.log("Hey");
-// }
-
-// const getName1 = () => {
-//   console.log("Hey, with const arrow function");
-// };
-
-// let getName2 = () => {
-//   console.log("Hey, with let arrow function");
-// };
-
-// var getName3 = () => {
-//   console.log("Hey, with var arrow function");
-// };
-
-// const getiDS = function () {
-//   console.log("Hey");
-// };
-// 2X+3Y+Z =20; // EXPRESSION
-
-// null undefined
-
-//undefined means value which is not defined
-// undefined is flasy value
-//typeof undefined =>
-// this value is assigned by the compiler at the time of memor allocation
-console.log(typeof undefined); // undefined
-
-var aaa; // undefined
-
-// Null means falsy value
-// it s assigned by developer
-// its a one type of value
-// typeof null
-console.log(typeof null);
-
-console.log("==", null == undefined);
-console.log("==", true == "vishal");
-
-console.log("===", null === undefined);
+console.log(Myname.arguments);
--- practise12.js
+++ practise12.js
@@ -1,189 +1,242 @@
-Array.prototype.myReduce = function (callbackFunction, initial) {
-  this.forEach((elem) => {
-    initial =
-      typeof initial !== "undefined" ? callbackFunction(initial, elem) : elem;
-  });
-  return initial;
-};
-
-Array.prototype.myReduce = function (cb, initialValue) {
-  // Complete the implementation of myReduce
-  let ans = initialValue || (this[0] ? this[0] : undefined);
-  if (ans !== undefined) {
-    for (let i = initialValue ? 0 : 1; i < this.length; i++) {
-      ans = cb(ans, this[i], i, this);
-    }
-  }
-  return ans;
-};
-const data = [1].myReduce((acc, value) => {
-  return acc + value;
-}, 0);
-
-const data22 = [1].reduce((acc, value) => {
-  return acc + value;
-}, 0);
-
-console.log(data, "hey");
-console.log(data22);
-
-const employee = [1, 2, 3, 4, 5, 6];
-
-const shubhamServant = (value) => {
-  return value * 2;
-};
-
-const updatedEmployee = employee.map(shubhamServant);
-
-console.log(updatedEmployee); // [2,4,6,8,10,12]
-
-// delclarative approach and imperative approach
-
-// anywhere you will go through
-
-// imperative approach
-for (let i = 5; i < employee; i = i + 2) {}
-
-// declarative approach
-// map, reduce , for..of , for...in , forEach , filter
-
-employee.forEach((value) => {
-  console.log(value);
-});
-
-// array find ??
-
-// const updatedemployeeData = [
-//   { name: "Chim chom", salary: 400000, isManager: false, isLead: true },
-//   { name: "him hom", salary: 480000, isManager: false, isLead: true },
-//   { name: "tir tor", salary: 460000, isManager: false, isLead: true },
-//   { name: "tir tor", salary: 560000, isManager: true, isLead: false },
-//   { name: "tir tor", salary: 660000, isManager: true, isLead: false },
-// ];
-//lead
-//manager
-
-// filter out the employee whose salary is greater than  or equal to 400000
-// and if salary is greater than 500000 and one key on object which will be isManager and add true
-// isLead
-const getEmployee = (value) => {
-  return value.salary === 400000;
-};
-
-// const targetObject = employeeData.find(getEmployee);
-
-// console.log(targetObject); // { name: "Chim chom", salary: 400000 }
-
-// chaining on array hoc
-
-// i  do not need the data whose salary is less than 4 lakh
-const employeeData = [
-  { name: "Chim chom", salary: 400000 }, // => 0
-  { name: "rim rom", salary: 320000 }, // => 1
-  { name: "him hom", salary: 480000 }, // 2
-  { name: "tir tor2", salary: 460000 }, // 3
-  { name: "tir tor1", salary: 560000 }, //4
-  { name: "tir tor3", salary: 660000 }, //5
-];
-
-const filteredData = employeeData
-  .filter((v) => {
-    return v.salary >= 400000;
-  })
-  .map((value) => {
-    if (value.salary > 500000) {
-      return {
-        ...value,
-        isManager: true,
-        isLead: false,
-      };
-    } else {
-      return {
-        ...value,
-        isManager: false,
-        isLead: true,
-      };
-    }
-  });
-
-//  const minimizedData = filteredData.map((value) => {
-// if (value.salary > 500000) {
-//   return {
-//     ...value,
-//     isManager: true,
-//     isLead: false,
-//   };
-// } else {
-//   return {
-//     ...value,
-//     isManager: false,
-//     isLead: true,
-//   };
-// }
-// });
-console.log("filteredData", filteredData); //
-// console.log("minimizedData", minimizedData); //
-
-// filter ??
-
-// bind ??
-// function securityProvider(...name) {
-//   console.log(this); // context
-//   console.log(name); //
-// }
-
-// const security1 = securityProvider.bind(
-//   { grandFather: "Rim Rom" },
-//   "Ram",
-//   "tikor"
-// );
-
-// const security2 = securityProvider.bind({ pet: "peti reti" }, "Tek", "pikor");
-
-// security1("XYZ", 123, 456); //  []
-// security2("ABC");
-
-// //  my bind
-
-// Function.prototype.mybind = function (obj, ...args) {
-//   // this that will be the function which is using mybind or function before dot
-//   const copyObject = { ...obj }; // deep copy
-//   copyObject.func = this;
-//   return (...arg2) => {
-//     const arg = [...args, ...arg2];
-//     return copyObject.func(arg);
-//   };
+// Array.prototype.myReduce = function (callbackFunction, initial) {
+//   this.forEach((elem) => {
+//     initial =
+//       typeof initial !== "undefined" ? callbackFunction(initial, elem) : elem;
+//   });
+//   return initial;
 // };
 
-// var security222 = securityProvider.mybind({ pet: "peti reti" }, "Tek", "pikor");
+// Array.prototype.myReduce = function (cb, initialValue) {
+//   // Complete the implementation of myReduce
+//   let ans = initialValue || (this[0] ? this[0] : undefined);
+//   if (ans !== undefined) {
+//     for (let i = initialValue ? 0 : 1; i < this.length; i++) {
+//       ans = cb(ans, this[i], i, this);
+//     }
+//   }
+//   return ans;
+// };
+// const data = [1].myReduce((acc, value) => {
+//   return acc + value;
+// }, 0);
+
+// const data22 = [1].reduce((acc, value) => {
+//   return acc + value;
+// }, 0);
+
+// console.log(data, "hey");
+// console.log(data22);
+
+// const employee = [1, 2, 3, 4, 5, 6];
+
+// const shubhamServant = (value) => {
+//   return value * 2;
+// };
+
+// const updatedEmployee = employee.map(shubhamServant);
+
+// console.log(updatedEmployee); // [2,4,6,8,10,12]
+
+// // delclarative approach and imperative approach
+
+// // anywhere you will go through
+
+// // imperative approach
+// for (let i = 5; i < employee; i = i + 2) {}
+
+// // declarative approach
+// // map, reduce , for..of , for...in , forEach , filter
+
+// employee.forEach((value) => {
+//   console.log(value);
+// });
+
+// // array find ??
+
+// // const updatedemployeeData = [
+// //   { name: "Chim chom", salary: 400000, isManager: false, isLead: true },
+// //   { name: "him hom", salary: 480000, isManager: false, isLead: true },
+// //   { name: "tir tor", salary: 460000, isManager: false, isLead: true },
+// //   { name: "tir tor", salary: 560000, isManager: true, isLead: false },
+// //   { name: "tir tor", salary: 660000, isManager: true, isLead: false },
+// // ];
+// //lead
+// //manager
+
+// // filter out the employee whose salary is greater than  or equal to 400000
+// // and if salary is greater than 500000 and one key on object which will be isManager and add true
+// // isLead
+// const getEmployee = (value) => {
+//   return value.salary === 400000;
+// };
+
+// // const targetObject = employeeData.find(getEmployee);
+
+// // console.log(targetObject); // { name: "Chim chom", salary: 400000 }
+
+// // chaining on array hoc
+
+// // i  do not need the data whose salary is less than 4 lakh
+// const employeeData = [
+//   { name: "Chim chom", salary: 400000 }, // => 0
+//   { name: "rim rom", salary: 320000 }, // => 1
+//   { name: "him hom", salary: 480000 }, // 2
+//   { name: "tir tor2", salary: 460000 }, // 3
+//   { name: "tir tor1", salary: 560000 }, //4
+//   { name: "tir tor3", salary: 660000 }, //5
+// ];
+
+// const filteredData = employeeData
+//   .filter((v) => {
+//     return v.salary >= 400000;
+//   })
+//   .map((value) => {
+//     if (value.salary > 500000) {
+//       return {
+//         ...value,
+//         isManager: true,
+//         isLead: false,
+//       };
+//     } else {
+//       return {
+//         ...value,
+//         isManager: false,
+//         isLead: true,
+//       };
+//     }
+//   });
+
+// //  const minimizedData = filteredData.map((value) => {
+// // if (value.salary > 500000) {
+// //   return {
+// //     ...value,
+// //     isManager: true,
+// //     isLead: false,
+// //   };
+// // } else {
+// //   return {
+// //     ...value,
+// //     isManager: false,
+// //     isLead: true,
+// //   };
+// // }
+// // });
+// console.log("filteredData", filteredData); //
+// // console.log("minimizedData", minimizedData); //
+
+// // filter ??
+
+// // bind ??
+// // function securityProvider(...name) {
+// //   console.log(this); // context
+// //   console.log(name); //
+// // }
+
+// // const security1 = securityProvider.bind(
+// //   { grandFather: "Rim Rom" },
+// //   "Ram",
+// //   "tikor"
+// // );
+
+// // const security2 = securityProvider.bind({ pet: "peti reti" }, "Tek", "pikor");
 
 // // security1("XYZ", 123, 456); //  []
+// // security2("ABC");
 
-// security222("security222", "ABC");
+// // //  my bind
 
-// what is the diffrenece b/w forEach and map ??
-var b = 50;
+// // Function.prototype.mybind = function (obj, ...args) {
+// //   // this that will be the function which is using mybind or function before dot
+// //   const copyObject = { ...obj }; // deep copy
+// //   copyObject.func = this;
+// //   return (...arg2) => {
+// //     const arg = [...args, ...arg2];
+// //     return copyObject.func(arg);
+// //   };
+// // };
 
-function callMeRelax() {
-  const a = (b = c = d = e = f = 50);
-  tit = "name";
-  console.log(a, b, c, d, e, f); // 50 ,50 ,50 ,50,50,50
+// // var security222 = securityProvider.mybind({ pet: "peti reti" }, "Tek", "pikor");
+
+// // // security1("XYZ", 123, 456); //  []
+
+// // security222("security222", "ABC");
+
+// // what is the diffrenece b/w forEach and map ??
+// var b = 50;
+
+// function callMeRelax() {
+//   const a = (b = c = d = e = f = 50);
+//   tit = "name";
+//   console.log(a, b, c, d, e, f); // 50 ,50 ,50 ,50,50,50
+// }
+// callMeRelax();
+// console.log(tit);
+// console.log(b, c, d, e, f);
+
+// console.log(a, b, c, d, e, f); // ??
+
+// // tit ='Tname';
+// var tit = "Tname";
+
+// // what is type coersion ??
+// // what is type casting
+// // what is function expression and function statement in term hositing?
+// // what is scope  and scope chainign ??
+// // what is lexical environment?
+
+// // what is memoization and write the function for memoizing value ?
+// const data1213 = `{} , {} , {} , {}`;
+// const array12e = data1213.split();
+
+function callMe(params, params2) {
+  console.log("hery");
 }
-callMeRelax();
-console.log(tit);
-console.log(b, c, d, e, f);
 
-console.log(a, b, c, d, e, f); // ??
+//this  => corresponding to ther ece
+// global
+//  local
+// console.log(callMe);
 
-// tit ='Tname';
-var tit = "Tname";
+// window = null;
+// this = window;
+// console.log(window === this);
 
-// what is type coersion ??
-// what is type casting
-// what is function expression and function statement in term hositing?
-// what is scope  and scope chainign ??
-// what is lexical environment?
+Function.prototype.mycall = function (obj, ...args) {
+  // this will be the type of function
+  //  obj  = {
+  //   name: "Visha;",
+  // getName: function() {
+  //   console.log(this); //=> object
+  // }
+  // };
 
-// what is memoization and write the function for memoizing value ?
-const data1213 = `{} , {} , {} , {}`;
-const array12e = data1213.split();
+  console.log(args); // [12, 13, 14]
+  obj.func = this;
+  obj.func(...args);
+};
+
+Function.prototype.myapply = function (obj, args) {
+  // this will be the type of function
+  //  obj  = {
+  //   name: "Visha;",
+  // getName: function() {
+  //   console.log(this); //=> object
+  // }
+  // };
+
+  console.log(args); // [12, 13, 14]
+  obj.func = this;
+  obj.func(...args);
+};
+
+// console.log(callMe.mycall());
+
+const object = {
+  name: "Visha;",
+};
+
+function getName() {
+  console.log(this); //=> window
+}
+
+getName.mycall(object, [12, 13, 14]);
