39992707221f314f0aba6d978d6b7b9bff945d17
Vishal Sharma <785vishal443@gmail.com>
2024-07-20 11:14:38 +0530

    function declaration

--- Practise/index.js
+++ Practise/index.js
@@ -6,10 +6,33 @@
 // Prototype
 
 
-var a=30;
-console.log(a)
-let b=90;
-console.log(b)
+// var a=30;
+// console.log(a)
+// let b=90;
+// console.log(b)
 
-// node install 
-// in global system
+// // node install 
+// // in global system
+
+
+
+// console.log(a)
+// var a=60;
+
+
+
+// console.log(b)
+
+// let b=30;
+
+
+//In javascript hoisting is creating a memory or assigning a memory for the variable let var and cosnt and function as well
+// Due to hoisting var variable and normal function can access before declaration , or we can say they are accessible top of it declaration
+// So we can say all variable are hoisted like let var , const and function , var and function are accessible befoe declaration
+// let and const are not accessible before declaration because they are in tdz for that time.
+
+call()
+
+function call (){
+  console.log("Hey chill")
+}
