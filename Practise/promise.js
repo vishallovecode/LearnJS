ac302401a84e87b82bf5912bddfce97e0eebc63c
Vishal Sharma <785vishal443@gmail.com>
2024-07-21 11:24:49 +0530

    fetch api call then and catch

--- Practise/promise.js
+++ Practise/promise.js
@@ -37,51 +37,84 @@
 
 
 // function success (res) {
-//   console.log('inside success:',res)
-// }
+// //   console.log('inside success:',res)
+// // }
 
-// function error(error) {
-//   console.log('inside error:' ,error)
-// }
+// // function error(error) {
+// //   console.log('inside error:' ,error)
+// // }
 
-// pr1.then(success , error)
-// pr1.then((res)=>{
-//   console.log(res)
-// } , (err)=>{
-//   console.log(err)
+// // pr1.then(success , error)
+// // pr1.then((res)=>{
+// //   console.log(res)
+// // } , (err)=>{
+// //   console.log(err)
+// // })
+
+
+// const pr2 = new Promise((resolved, rejected)=>{
+//   resolved(2)
+// })
+
+// // const data  = pr2.then((res)=>{
+// //   console.log(res) // 2
+// //   return res; // 
+// // })
+
+// // console.log('data', data) // 2
+
+
+// pr2.then((res)=>{
+//   return res;
+// }).then((res)=>{
+//   return res*10
+// }).then((res)=>{
+//   console.log('2',res*100) // 2000
+//   return res*100;
+// }).catch((error)=>{
+//   return 10;
+// }).then((res)=>{
+//    console.log('1',res/20) // 100
 // })
 
 
-const pr2 = new Promise((resolved, rejected)=>{
-  resolved(2)
-})
-
-// const data  = pr2.then((res)=>{
-//   console.log(res) // 2
-//   return res; // 
-// })
-
-// console.log('data', data) // 2
-
-
-pr2.then((res)=>{
-  return res;
-}).then((res)=>{
-  return res*10
-}).then((res)=>{
-  console.log('2',res*100) // 2000
-  return res*100;
-}).catch((error)=>{
-  return 10;
-}).then((res)=>{
-   console.log('1',res/20) // 100
-})
-
-
-
-
+// then().
+// fetch() // promise return  
 
   
+
+// async and await  
+// this is use for resolving the promise
+
+function fetchData() {
+  // fetch return the promise
+ fetch('https://dummyjson.com/products').then((res)=>{
+  // res is not in json type
+  return res.json(); // this will also return the promise
+ }).then((result)=>{
+  console.log(result)
+ }).catch((error)=>{
+  console.log('api', error)
+ });
+
+}
+
+
+fetchData()
+
+
+// function fetch(url) {
+//   // hitting the server taking a response
+//   return new Promise((resolved, rejected)=>{
+//     // lets response is result
+//     // status is  resStatus
+//     if(resStatus ==='success') {
+//       resolved(result)
+//     }else {
+//       rejected('Error:')
+//     }
+//   })
+// }
   
   
   
